7fb5714884f01f49fb40fec96f58fb6552b2893c
仿生狮子 <tangnad@qq.com>
2024-11-28 10:07:40 +0800

    feat: 记录标题层级以及初步处理笔记

--- transform_notes/index.js
+++ transform_notes/index.js
@@ -5,28 +5,49 @@
 const fs = require('fs')
 const path = require('path')
 
+// 简读日本史
 const bookID = '59c328507273b68f59cb9d9'
-const bookDir = path.resolve(__dirname, `../cache/${bookID}`)
-const metaDir = path.resolve(bookDir, 'meta.json')
+const bookCacheDir = path.resolve(__dirname, `../cache/${bookID}`)
+const metaDir = path.resolve(bookCacheDir, 'meta.json')
 
 const metaContent = fs.readFileSync(metaDir, 'utf-8')
 const meta = JSON.parse(metaContent)
 
-const notesDir = path.resolve(bookDir, 'notes.txt')
+const notesDir = path.resolve(bookCacheDir, 'notes.txt')
 const notesContent = fs.readFileSync(notesDir, 'utf-8')
 
-const titles = meta.chapters.map(x => x.title).map(x => x.trim())
+const titles = [] // { title: 'xxx', level: 2 }
+
+meta.chapters.map(chapter => {
+  titles.push({
+    title: chapter.title,
+    level: chapter.level
+  })
+  if (chapter.anchors.length) {
+    titles.push(...chapter.anchors)
+  }
+})
 
 const results = []
 notesContent
   .split('\n')
   .filter(Boolean)
   .map((line, _idx) => {
-    const result = { type: '', mark: '', comment: '', time: '' }
-    line = line.trim()
-    if (titles.find(title => title === line)) {
+    const result = { type: '', mark: '', comment: '', time: '', data: '' }
+    line = line
+      .trim()
+      // 移除奇怪的字符如零宽空格
+      .replace(/\u200b/g, "")
+      // 移除不完整的页码（左）
+      .replace(/^\d*\]/, '')
+      // 移除不完整的页码（右）
+      .replace(/\[\d*$/, '')
+
+    const findTitle = titles.find(x => x.title === line)
+    if (findTitle) {
       result.type = 'title'
-      result.mark = line
+      result.mark = findTitle.title
+      result.data = findTitle.level
     } else if (line.match(/^◆ (\d{4}\/\d{2}\/\d{2})发表想法/)) {
       result.type = 'comment'
       result.time = line.match(/^◆ (\d{4}\/\d{2}\/\d{2})发表想法/)[1]
@@ -55,5 +76,5 @@ results
     return note
   })
 
-const notesMetaDir = path.resolve(bookDir, 'notes.json')
+const notesMetaDir = path.resolve(bookCacheDir, 'notes.json')
 fs.writeFileSync(notesMetaDir, JSON.stringify(results, null, 2))
